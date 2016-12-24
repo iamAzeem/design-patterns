14e5f270c81fffe0ef1419e43b3c12013b359c12
Azeem Sajid <iamAzeem@users.noreply.github.com>
2016-12-24 17:40:40 +0500

    Singleton on stack example

---
+++ singleton_stack.cpp
@@ -0,0 +1,43 @@
+// Singleton Pattern - On Stack
+
+#include <iostream>
+
+namespace DP {
+
+class Singleton
+{
+public:
+	static Singleton* getInstance()
+	{
+		static Singleton instance;
+		return &instance;
+	}
+	
+	~Singleton()
+	{
+		std::cout << "Singleton destroyed from stack!" << std::endl;
+	}
+	
+	void hello()
+	{
+		std::cout << "Hello from Singleton!" << std::endl;
+	}
+	
+private:
+	explicit Singleton()
+	{
+		std::cout << "Singleton created on stack!" << std::endl;
+	}
+};
+
+} // DP
+
+int main() 
+{
+	using namespace DP;
+	
+	auto obj = Singleton::getInstance();
+	obj->hello();
+	
+	return 0;
+}
